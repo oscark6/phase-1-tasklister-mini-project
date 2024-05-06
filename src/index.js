3251590495bab9e6fe29151434748971e29e9705
oscark6 <oscar.kimani1@student.moringaschool.com>
2024-05-06 18:06:10 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const selectForm = document.querySelector('form');
+
+  selectForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const description = e.target.querySelector('#new-task-description').value
+
+    handleToDo(description);
+
+    selectForm.reset();
+  })
 });
+
+  function handleToDo(description){
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    li.textContent = description;
+
+    deleteButton.textContent = 'x';
+
+
+
+    li.appendChild(deleteButton);
+
+    document.querySelector('#tasks').appendChild(li);
+
+
+    deleteButton.addEventListener('click', handleDelete);
+  }
+
+  function handleDelete(e){
+
+    e.target.parentNode.remove();
+  }
